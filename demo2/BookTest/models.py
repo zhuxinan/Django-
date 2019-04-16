5fe9930385814802a59d9588cd32ff8f6ed195f8
zhuxinan <18238955523qq>
2019-04-16 21:08:14 +0800

    创建demo2的模型

--- demo2/BookTest/models.py
+++ demo2/BookTest/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    bname = models.CharField(max_length=20)
+    bauthor = models.CharField(max_length=20)
+    bcontent = models.CharField(max_length=100)
+    def __str__(self):
+        return self.bname
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=100)
+    def __str__(self):
+        return self.hname
