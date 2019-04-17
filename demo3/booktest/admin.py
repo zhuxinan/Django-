a00b72c6cbc741b566b22935a959c0bce855f9ae
zhuxinan <18238955523qq>
2019-04-17 20:07:20 +0800

    demo3自定义管理页面、关联注册与后台对象列名更改

--- demo3/booktest/admin.py
+++ demo3/booktest/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import BookInfo,HeroInfo
+
+class HeroInfoInline(admin.StackedInline):
+    model = HeroInfo
+    extra = 1
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['bookid','bookname','bookauthor','bookcontent']
+    list_filter = ['bauthor',]
+    search_fields = ['bauthor','id','bname']
+    list_per_page = 2
+    inlines = [HeroInfoInline]
+
+admin.site.register(BookInfo,BookInfoAdmin)
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ['heroid','heroname','herogender','herocontent']
+    list_filter = ['hname','hgender','hcontent']
+    search_fields = ['hname','hgender','hcontent']
+    list_per_page = 2
+
+admin.site.register(HeroInfo,HeroInfoAdmin)
--- demo3/booktest/models.py
+++ demo3/booktest/models.py
@@ -8,6 +8,18 @@ class BookInfo(models.Model):
     bcontent = models.CharField(max_length=100)
     def __str__(self):
         return self.bname
+    def bookname(self):
+        return self.bname
+    bookname.short_description = '书籍名称'
+    def bookauthor(self):
+        return self.bauthor
+    bookauthor.short_description = '书籍作者'
+    def bookcontent(self):
+        return self.bcontent
+    bookcontent.short_description = '书籍简介'
+    def bookid(self):
+        return self.id
+    bookid.short_description = '书籍编号'
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)
@@ -16,3 +28,15 @@ class HeroInfo(models.Model):
     hbook = models.ForeignKey('BookInfo',on_delete=models.CASCADE)
     def __str__(self):
         return self.hname
+    def heroname(self):
+        return self.hname
+    heroname.short_description = '人物名称'
+    def herogender(self):
+        return self.hgender
+    herogender.short_description = '人物性别'
+    def herocontent(self):
+        return self.hcontent
+    herocontent.short_description = '人物简介'
+    def heroid(self):
+        return self.id
+    heroid.short_description = '人物编号'
